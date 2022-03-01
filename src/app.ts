476236f7061ddf1f57a31130842d02ba3678fa91
Joao Harbs <harbspj@gmail.com>
2022-03-01 18:28:01 +0100

    chore: added functions to register services on app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import IConfig from '@interfaces/config';
+import { IConfig, IService } from '@interfaces/common';
 import { autoloadConfig, getBaseDir } from '@utils/helper';
-import { get } from 'lodash';
+import _, { get } from 'lodash';
 
 class App {
     private configObject: IConfig | unknown;
+    public services: { [key: string]: ThisType<unknown> }[] = [];
 
     constructor(base_dir: null | string = null) {
         this.configObject = base_dir ? autoloadConfig(getBaseDir()) : {};
@@ -19,6 +20,38 @@ class App {
         if (!key) return this.configObject;
 
         return get(this.configObject, key, _default);
+    }
+
+    /**
+     * has: Verify if service exists.
+     * @param service string
+     * @returns boolean
+     */
+    has(service: string): boolean {
+        return _.keys(this.services).includes(service);
+    }
+
+    /**
+     * get: Get a service.
+     * @param service string
+     * @returns T
+     */
+    get<T = any>(service: string, exception = false): T {
+        if (!this.has(service) && exception)
+            throw `service ${service} not initialized`;
+
+        return this.services[service];
+    }
+
+    /**
+     * add: Add a service.
+     * @param name string
+     * @param concrete Function | IService
+     */
+    add(name: string, concrete: () => void | IService | unknown): App {
+        this.services[name] = concrete;
+
+        return this;
     }
 
     async start(server: () => void): Promise<void> {
